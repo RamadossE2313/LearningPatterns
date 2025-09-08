e5790f0a3bb2c77fcd57579e9293506edef46818
Ramadoss E <Rishiram2313@gmail.com>
2025-09-09 00:41:32 +0530

    Singleton pattern added

---
+++ A- Creational Patterns/1- Singleton Pattern/SignletonPattern/Program.cs
@@ -0,0 +1,38 @@
+﻿using SignletonPattern;
+
+//var instance1 = Singleton.Instance;
+//instance1.SomeBusinessLogic();
+
+//var instance2 = Singleton.Instance;
+//instance2.SomeBusinessLogic();
+
+
+//var instance1 = SingletonWithLazy.Instance;
+//instance1.SomeBusinessLogic();
+
+//var instance2 = SingletonWithLazy.Instance;
+//instance2.SomeBusinessLogic();
+
+
+//var instance1 = SingletonWithLock.Instance;
+//instance1.SomeBusinessLogic();
+
+//var instance2 = SingletonWithLock.Instance;
+//instance2.SomeBusinessLogic();
+
+var instance1 = SingletonGeneric<Logger>.Instance;
+Console.WriteLine(instance1.LogInformation());
+
+var instance2 = SingletonGeneric<Logger>.Instance;
+Console.WriteLine(instance2.LogInformation());
+
+if (instance1 == instance2)
+    Console.WriteLine("yes");
+
+Console.ReadLine();
+
+
+public class Logger
+{
+    public string LogInformation() => "Log Information";
+}
---
+++ A- Creational Patterns/1- Singleton Pattern/SignletonPattern/Singleton.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignletonPattern
+{
+    // Singleton Pattern ensures a class has only one instance and provides a global point of access to it.
+    public sealed class Singleton
+    {
+        private static Singleton? _instance;
+
+        private Singleton()
+        {
+
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+
+        public void SomeBusinessLogic() => Console.WriteLine("SomeBusinessLogic method called");
+    }
+
+
+    //Before lazy how we handled thread safety using lock
+
+    public sealed class SingletonWithLock
+    {
+        private static SingletonWithLock? _instance;
+        private static readonly object _lock = new object();
+        private SingletonWithLock()
+        {
+
+        }
+        public static SingletonWithLock Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new SingletonWithLock();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public void SomeBusinessLogic() => Console.WriteLine("SomeBusinessLogic method called");
+    }
+
+
+    // Lazy Initialization, it will help thread safety
+
+    public sealed class SingletonWithLazy
+    {
+        private static readonly Lazy<SingletonWithLazy> _instance = new Lazy<SingletonWithLazy>(() => new SingletonWithLazy());
+
+        private SingletonWithLazy()
+        {
+
+        }
+        public static SingletonWithLazy Instance => _instance.Value;
+
+        public void SomeBusinessLogic() => Console.WriteLine("SomeBusinessLogic method called");
+    }
+
+    // Generic way of creating
+
+    public sealed class SingletonGeneric<T> where T : new()
+    {
+        private static readonly Lazy<T> _instance =
+            new(() => new T());
+
+        public static T Instance => _instance.Value;
+
+        private SingletonGeneric() { }
+
+        public void SomeBusinessLogic() => Console.WriteLine("SomeBusinessLogic method called");
+    }
+}
